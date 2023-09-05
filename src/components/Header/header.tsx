195156dad8fb033b6ea54718b2b6e4a4c0cf0757
Thiago Carpinetti <thiac12@gmail.com>
2023-09-05 14:01:21 -0300

    feat: :sparkles: Navigation bar, styles adjustments

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,24 +3,31 @@ import styles from './header.module.css';
 
 function Header() {
   return (
-    <header className={ styles.Header }>
-      <div><h1>Portfólio</h1></div>
-      <nav>
-        <a
-          href="https://github.com/Carpinett1"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <AiFillGithub size="2rem" color="#FFFCF7" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/thiagocarpinetti/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <AiFillLinkedin size="2rem" color="#FFFCF7" />
-        </a>
-      </nav>
+    <header>
+      <div className={ styles.Header }>
+        <div><h1>Portfólio</h1></div>
+        <nav>
+          <a
+            href="https://github.com/Carpinett1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className={ styles.img } />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/thiagocarpinetti/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin className={ styles.img } />
+          </a>
+        </nav>
+      </div>
+      <div className={ styles.pageNavigation }>
+        <a href="#about">Sobre mim</a>
+        <a href="#projects">Projetos</a>
+        <a href="#contact">Contato</a>
+      </div>
     </header>
   );
 }
