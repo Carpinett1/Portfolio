1753c2200bf7c971f1a947982ad265b336f8e47f
Thiago Carpinetti <thiac12@gmail.com>
2023-09-23 00:30:53 -0300

    feat: :sparkles: Initiating Projects Container

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import About from './components/About/about';
 import Footer from './components/Footer/footer';
 import Header from './components/Header/header';
+import Projects from './components/Projects/projects';
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Projects />
       <Footer />
     </>
   );
---
+++ src/components/Projects/projects.tsx
@@ -0,0 +1,9 @@
+import styles from './projects.module.css';
+
+function Projects() {
+  return (
+    <div className={ styles.container } style={ { height: '100vh' } } />
+  );
+}
+
+export default Projects;
