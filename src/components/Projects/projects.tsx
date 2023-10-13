67e8c259bb02bb3a02d84246ec5c0ab3f715ac86
Thiago Carpinetti <thiac12@gmail.com>
2023-10-13 03:50:01 -0300

    feat: :sparkles: Introducing ProjectsCards, a better way to view projects i worked in, with links and images

--- src/components/Projects/projects.tsx
+++ src/components/Projects/projects.tsx
@@ -1,8 +1,39 @@
+import { TbSourceCode, TbExternalLink } from 'react-icons/tb';
 import styles from './projects.module.css';
+import Data from '../../database/ProjectsData';
 
 function Projects() {
   return (
-    <div className={ styles.container } style={ { height: '100vh' } } />
+    <div id="projects" className={ styles.ProjectsContainer }>
+      {Data && Data.Projects.map((project) => (
+        <div className={ styles.ProjectCard } key={ project.id }>
+          <div className={ styles.imgContainer }>
+            <img className={ styles.img1 } src={ project.img[0] } alt={ project.name } />
+            <img className={ styles.img2 } src={ project.img[1] } alt={ project.name } />
+          </div>
+          <div className={ styles.InfoContainer }>
+            <h1>{project.name}</h1>
+            <h2>{project.description}</h2>
+            <nav>
+              <a
+                href={ project.github }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <TbSourceCode className={ styles.code } />
+              </a>
+              <a
+                href={ project.link }
+                target="_blank"
+                rel="noreferrer"
+              >
+                <TbExternalLink className={ styles.code } />
+              </a>
+            </nav>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
---
+++ src/database/ProjectsData.ts
@@ -0,0 +1,71 @@
+import Store1 from '../assets/Projects/Store1.png';
+import Store2 from '../assets/Projects/Store2.png';
+
+import Wallet1 from '../assets/Projects/Wallet1.png';
+import Wallet2 from '../assets/Projects/Wallet2.png';
+
+import Itunefy1 from '../assets/Projects/Itunefy1.png';
+import Itunefy2 from '../assets/Projects/Itunefy2.png';
+
+import Password1 from '../assets/Projects/Password1.png';
+import Password2 from '../assets/Projects/Password2.png';
+
+type ProjectType = {
+  name: string,
+  link: string,
+  github: string,
+  img: string[],
+  description: string,
+  id: number,
+};
+
+type DataProps = {
+  Projects: ProjectType[]
+};
+
+const Data: DataProps = {
+  Projects: [
+    {
+      name: 'Front End Store',
+      link: 'https://frontendstoreweb.vercel.app/',
+      github: 'https://github.com/Carpinett1/FrontEnd-Store',
+      img: [Store1, Store2],
+      description:
+      'Um projeto desenvolvido com React e Redux onde utilizo uma'
+      + ' API do Mercado Livre para simular um website de uma loja online.',
+      id: 1,
+    },
+    {
+      name: 'Trybe Wallet',
+      link: 'https://trybewalletweb.vercel.app/',
+      github: 'https://github.com/Carpinett1/TrybeWallet',
+      img: [Wallet2, Wallet1],
+      description:
+      'Um projeto desafio feito pela Trybe, Que consiste em criar uma'
+      + ' carteira de despesas que utiliza cotação de moedas reais.',
+      id: 2,
+    },
+    {
+      name: 'Itunefy',
+      link: 'https://itunefy.vercel.app/',
+      github: 'https://github.com/Carpinett1/Itunefy-songApp',
+      img: [Itunefy1, Itunefy2],
+      description:
+      'Um projeto com inspiração no spotify e utilizando uma API do Itunes'
+      + ' para Pesquisar, Ouvir e Favoritar sua músicas.',
+      id: 3,
+    },
+    {
+      name: 'Password Manager',
+      link: 'https://passwordmanagerweb.vercel.app/',
+      github: 'https://github.com/Carpinett1/PasswordManager',
+      img: [Password1, Password2],
+      description:
+      'Uma aplicação simples para salvar suas senhas com segurança, Salvas apenas'
+      + ' localmente.',
+      id: 4,
+    },
+  ],
+};
+
+export default Data;
